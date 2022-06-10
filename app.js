32fc9438d73036e4a22c869011b9e15b8904e50f
= <stx36@mail.ru>
2022-06-10 19:08:51 -0300

    BOOKS

---
+++ app.js
@@ -0,0 +1,64 @@
+const saveLetter = document.querySelector('.save-letter')
+const list = document.querySelector('.list')
+
+const textArea = document.querySelector('textarea')
+const letterPosts = []
+let letter = {}
+let count = 1
+
+
+saveLetter.addEventListener('click', function(e){
+    const delLetter = document.querySelector('.removeLetter')
+    let lastElem = list.children.length + 1
+    let text = textArea.value
+
+
+    let li = document.createElement('li')
+    let span = document.createElement('span')
+    span.setAttribute('class', 'removeLetter')
+    span.innerHTML = 'x'
+    li.setAttribute('class', 'list-item')
+    li.setAttribute('data-key', lastElem)
+    li.innerHTML = `Запись ${+lastElem}`
+    li.appendChild(span) 
+    list.appendChild(li)
+
+    console.log(li);
+
+    //Добавление в LocalStorage
+    letter = {
+        key: +li.dataset.key,
+        value: text
+    }
+    letterPosts.push(letter)
+    localStorage.setItem('letter', JSON.stringify(letterPosts))
+    
+
+    // Выбор элемента
+    const listItems = list.children
+    for(let elem of listItems) {elem.addEventListener('click', choiceElem)}
+
+
+    console.log(delLetter);
+
+
+    count++
+    textArea.value = ''
+})
+
+
+
+
+
+
+function choiceElem() {
+    const data = localStorage.getItem('letter')
+    const arrData = JSON.parse(data)
+
+    for (let dataElem of arrData) {
+        if(+this.dataset.key === dataElem.key ){
+            textArea.value = dataElem.value
+            textArea.focus()
+        }
+    }
+}
